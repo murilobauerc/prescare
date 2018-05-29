9b422fc97d9f3d4f7d43a46a5b9a0cc0afc958ec
murilobauerc <panchobauer@gmail.com>
2018-05-29 11:50:06 -0300

    :rocket: <@murilobauerc, @marcos012> implementa bootstrap

--- public/js/tableFunctions.js
+++ public/js/tableFunctions.js
@@ -6,7 +6,11 @@ window.addEventListener('load', () => {
         if (table.getElementsByClassName('js-novo-elemento-pomada').item(0)) {
             tableObj = Table(table)
             tableObj.initializeButtonPomada()
-        } else {
+        } else if (table.getElementsByClassName('js-novo-elemento-cuidados').item(0)) {
+            tableObj = Table(table)
+            tableObj.initializeButtonCuidados()
+        }
+        else {
             tableObj = Table(table)
             tableObj.initializeButton()
         }
@@ -20,6 +24,7 @@ let Table = (tableContainer) => ({
     tableBody: tableContainer.getElementsByTagName('tbody').item(0),
     novoElForm: tableContainer.getElementsByClassName('js-novo-elemento').item(0),
     novoElFormPom: tableContainer.getElementsByClassName('js-novo-elemento-pomada').item(0),
+    novoElFormCuid: tableContainer.getElementsByClassName('js-novo-elemento-cuidados').item(0),
 
     appendRow(row) {
         this.tableBody.insertBefore(row, this.novoElForm)
@@ -27,8 +32,11 @@ let Table = (tableContainer) => ({
     appendRowPom(row) {
         this.tableBody.insertBefore(row, this.novoElFormPom)
     },
+    appendRowCuid(row) {
+        this.tableBody.insertBefore(row, this.novoElFormCuid)
+    },
 
-    createRow(novoMedicamento, novoVia, novoIntervalo, novoFormaFarmaceutica, novoObservacao, novoDataDePrescricao, ) {
+    createRow(novoMedicamento, novoVia, novoIntervalo, novoFormaFarmaceutica, novoObservacao, novoDataDePrescricao) {
         let row = document.createElement('tr')
 
         for (let arg of arguments) {
@@ -39,7 +47,7 @@ let Table = (tableContainer) => ({
 
         return row
     },
-    createRowPom(novoMedicamento, novoLocal, novoIntervalo, novoObservacao, novoDataDePrescricao, ) {
+    createRowPom(novoMedicamento, novoLocal, novoIntervalo, novoObservacao, novoDataDePrescricao) {
         let row = document.createElement('tr')
 
         for (let arg of arguments) {
@@ -49,7 +57,18 @@ let Table = (tableContainer) => ({
         }
 
         return row
-    }, 
+    },
+    createRowCuid(novoCuidado, novoPeriodo, novoHorario, novoObservacao, novoDataDePrescricao) {
+        let row = document.createElement('tr')
+
+        for (let arg of arguments) {
+            let td = document.createElement('td')
+            td.innerHTML = arg
+            row.appendChild(td)
+        }
+
+        return row
+    },
 
     initializeButton() {
         tableContainer.getElementsByClassName('js-add').item(0).addEventListener('click', () => {
@@ -59,7 +78,9 @@ let Table = (tableContainer) => ({
             let novoFormaFarmaceutica = this.novoElForm.querySelector('.js-novo-forma-farmaceutica').value
             let novoObservacao = this.novoElForm.querySelector('.js-novo-observacao').value
             let novoDataDePrescricao = this.novoElForm.querySelector('.js-novo-data-de-prescricao').value
-            this.appendRow(this.createRow(novoMedicamento.toUpperCase(), novoVia.toUpperCase(), novoIntervalo.toUpperCase(), novoFormaFarmaceutica.toUpperCase(), novoObservacao.toUpperCase(), novoDataDePrescricao.toUpperCase()))
+            this.appendRow(this.createRow(novoMedicamento.toUpperCase(), novoVia.toUpperCase(), novoIntervalo.toUpperCase(),
+                novoFormaFarmaceutica.toUpperCase(), novoObservacao.toUpperCase(),
+                novoDataDePrescricao.toUpperCase()))
 
             const clearAllInputs = (inputList) => inputList.forEach(input => input.value = "")
 
@@ -69,22 +90,23 @@ let Table = (tableContainer) => ({
             const clearVia = inputsByClassName('.js-novo-via');
             const clearInter = inputsByClassName('.js-novo-intervalo');
             const clearForma = inputsByClassName('.js-novo-forma-farmaceutica');
-            const clearObs = inputsByClassName('.js-novo-observacao');
+            const clearObservacao = inputsByClassName('.js-novo-observacao');
             const clearData = inputsByClassName('.js-novo-data-de-prescricao');
 
-            [clearMed(), clearVia(), clearInter(), clearForma(), clearObs(), clearData()]
+            [clearMed(), clearVia(), clearInter(), clearForma(), clearObservacao(), clearData()]
                 .forEach(clearAllInputs)
         })
     },
     initializeButtonPomada() {
         tableContainer.getElementsByClassName('js-add').item(0).addEventListener('click', () => {
-        
+
             let novoMedicamento = this.novoElFormPom.querySelector('.js-novo-medicamento').value
             let novoLocal = this.novoElFormPom.querySelector('.js-novo-local').value
             let novoIntervalo = this.novoElFormPom.querySelector('.js-novo-intervalo').value
             let novoObservacao = this.novoElFormPom.querySelector('.js-novo-observacao').value
             let novoDataDePrescricao = this.novoElFormPom.querySelector('.js-novo-data-de-prescricao').value
-            this.appendRowPom(this.createRowPom(novoMedicamento.toUpperCase(), novoLocal.toUpperCase(), novoIntervalo.toUpperCase(), novoObservacao.toUpperCase(), novoDataDePrescricao.toUpperCase()))
+            this.appendRowPom(this.createRowPom(novoMedicamento.toUpperCase(), novoLocal.toUpperCase(), novoIntervalo.toUpperCase(),
+                novoObservacao.toUpperCase(), novoDataDePrescricao.toUpperCase()))
 
             const clearAllInputs = (inputList) => inputList.forEach(input => input.value = "")
 
@@ -93,12 +115,37 @@ let Table = (tableContainer) => ({
             const clearMed = inputsByClassName('.js-novo-medicamento');
             const clearLocal = inputsByClassName('.js-novo-local');
             const clearInter = inputsByClassName('.js-novo-intervalo');
-            const clearObs = inputsByClassName('.js-novo-observacao');
+            const clearObservacao = inputsByClassName('.js-novo-observacao');
             const clearData = inputsByClassName('.js-novo-data-de-prescricao');
 
-            [clearMed(), clearLocal(), clearInter(), clearObs(), clearData()]
+            [clearMed(), clearLocal(), clearInter(), clearObservacao(), clearData()]
+                .forEach(clearAllInputs)
+        })
+    },
+
+    initializeButtonCuidados() {
+        tableContainer.getElementsByClassName('js-add').item(0).addEventListener('click', () => {
+
+            let novoCuidado = this.novoElFormCuid.querySelector('.js-novo-cuidado').value
+            let novoPeriodo = this.novoElFormCuid.querySelector('.js-novo-periodo').value
+            let novoHorario = this.novoElFormCuid.querySelector('.js-novo-horario').value
+            let novoObservacao = this.novoElFormCuid.querySelector('.js-novo-observacao').value
+            let novoDataDePrescricao = this.novoElFormCuid.querySelector('.js-novo-data-de-prescricao').value
+            this.appendRowCuid(this.createRowCuid(novoCuidado.toUpperCase(), novoPeriodo.toUpperCase(), novoHorario.toUpperCase(),
+                novoObservacao.toUpperCase(), novoDataDePrescricao.toUpperCase()))
+
+            const clearAllInputs = (inputList) => inputList.forEach(input => input.value = "")
+
+            const inputsByClassName = (className) => () => [...document.querySelectorAll(className)]
+
+            const clearCuid = inputsByClassName('.js-novo-cuidado');
+            const clearPeriodo = inputsByClassName('.js-novo-periodo');
+            const clearHorario = inputsByClassName('.js-novo-horario');
+            const clearObservacao = inputsByClassName('.js-novo-observacao');
+            const clearData = inputsByClassName('.js-novo-data-de-prescricao');
+
+            [clearCuid(), clearPeriodo(), clearHorario(), clearObservacao(), clearData()]
                 .forEach(clearAllInputs)
         })
     }
-
 })
