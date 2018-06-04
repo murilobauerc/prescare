3d19a5603176799d2c81dc77a7a1788ae5f0ca4f
marcos012 <marcospaulo.ad@gmail.com>
2018-06-04 16:07:08 -0300

    :construction: <@marcos012, @ClaudiaStrm> refaz rotas da tela medicamento

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,36 @@
-const prescricaoRoutes = require('./prescricao')
-const acolhidoRoutes = require('./acolhido')
-const applicationRoutes = require('./application')
-const medicamentoRoutes = require('./medicamento')
+
+const listaAcolhidos = require('../routes/listaAcolhidos')
+const home = require('../routes/home')
+const about = require('../routes/about')
+const acolhido = require('../routes/acolhido')
+const farmaceutica = require('../routes/farmaceutica')
+const prescricaoAtualizada = require('../routes/prescricaoAtualizada')
+
+const user = require('../mocks/user')
+const tabelaFarmaceutica = require('../mocks/tabelaFarmaceutica')
+const dadosFarmacia = require('../mocks/farmacia')
+
+const createPrescricao = require('./prescricao/create')
+const editPrescricao = require('./prescricao/edit')
+const getPrescricao = require('./prescricao/get')
+const destroyPrescricao = require('./prescricao/destroy')
+const updatePrescricao = require('./prescricao/update')
+const getMedicamento = require('./medicamento/get')
 
 const allRoutes = models => ({
-  acolhidoRoutes: acolhidoRoutes(models.Acolhido),
-  prescricaoRoutes: prescricaoRoutes(models.Prescricao),
-  medicamentoRoutes: medicamentoRoutes(models.Medicamento),
-  applicationRoutes: applicationRoutes()
+  listaAcolhidos: listaAcolhidos(models.Acolhido),
+  home: home(),
+  about: about(user),
+  acolhido: acolhido(models.Acolhido),
+  prescricaoAtualizada: prescricaoAtualizada(tabelaFarmaceutica),
+  farmaceutica: farmaceutica(dadosFarmacia),
+  medicamento: getMedicamento(models.Medicamento),
+
+  createPrescricao: createPrescricao(models.Prescricao),
+  editPrescricao: editPrescricao(models.Prescricao),
+  getPrescricao: getPrescricao(models.Prescricao),
+  destroyPrescricao: destroyPrescricao(models.Prescricao),
+  updatePrescricao: updatePrescricao(models.Prescricao)
 })
 
 module.exports = allRoutes
