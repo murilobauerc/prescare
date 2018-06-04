bbef737f70428c1f7f4d1c0f05c94ecf16532fc5
marcos012 <marcospaulo.ad@gmail.com>
2018-06-04 15:26:55 -0300

    :construction: <@marcos012, @ClaudiaStrm> adiciona rotas dos medicamentos

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,13 @@ const router = require('express').Router()
 const prescricaoRoutes = require('./prescricao')
 const acolhidoRoutes = require('./acolhido')
 const applicationRoutes = require('./application')
+const medicamentoRoutes = require('./medicamento')
 
 module.exports = models => {
   applicationRoutes(router)
   acolhidoRoutes(models.Acolhido, router)
   prescricaoRoutes(models.Prescricao, router)
-
+  medicamentoRoutes(models.Medicamento, router)
   return router;
 }
 
